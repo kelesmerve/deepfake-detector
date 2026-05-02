56924b638612fc52e841049c6c1b44ab9e62b167
mervekeles <kelesmerve742@gmail.com>
2026-05-02 18:49:25 +0300

    Gereksiz AASIST3 klasoru projeden ve GitHub'dan kaldirildi

--- AASIST3/model/__init__.py
+++
@@ -1,1 +0,0 @@
-from .full_model import aasist3
--- AASIST3/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .metrics import compute_antispoofing_metrics
-
-from .training_functions import train_one_epoch
-from .validation import compute_scores
